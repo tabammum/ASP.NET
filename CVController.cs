38368031ee90d2a688358e8d225f242194c5242a
tabammum <60968413+tabammum@users.noreply.github.com>
2022-09-23 01:06:30 +0600

    Add files via upload

---
+++ CVController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cv_assignment.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult CareerObjective()
+        {
+            return View();
+        }
+        public ActionResult Skills()
+        {
+            return View();
+        }
+        public ActionResult EducationalQualification()
+        {
+            return View();
+        }
+
+    }
+}
